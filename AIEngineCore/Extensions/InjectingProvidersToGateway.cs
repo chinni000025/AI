d476677a12436af30214f6f50ec7d482deddef1f
buggyveeravalli <veeravallibuggy@gmail.com>
2026-08-05 11:11:52 +0530

    Dynamic workers

--- AIEngineCore/Extensions/InjectingProvidersToGateway.cs
+++ AIEngineCore/Extensions/InjectingProvidersToGateway.cs
@@ -1,15 +1,9 @@
 ﻿namespace AIEngineCore.Extensions
 {
     using AIEngineConnectivity.EngineCore;
-    using AIEngineConnectivity.Services;
     using AIEngineCore.Providers;
     using AIEngineCore.Services;
-    using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
-    using System;
-    using System.Collections.Generic;
-    using System.Runtime.CompilerServices;
-    using System.Text;
 
     public static class InjectingProvidersToGateway
     {
