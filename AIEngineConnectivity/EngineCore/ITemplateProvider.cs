062537b52f599f6340d8a12c240fbd692189e370
buggyveeravalli <veeravallibuggy@gmail.com>
2026-08-05 16:43:24 +0530

    Move Template to the Gateway

---
+++ AIEngineConnectivity/EngineCore/ITemplateProvider.cs
@@ -0,0 +1,8 @@
+﻿namespace AIEngineConnectivity.EngineCore
+{
+    using AIEngineConnectivity.Constants;
+    public interface ITemplateProvider
+    {
+        Task<string> GetTemplate(EngineEvents @event, CancellationToken ct = default);
+    }
+}
---
+++ AIEngineCore/EngineCore/TemplateProvider.cs
@@ -0,0 +1,15 @@
+﻿namespace AIEngineCore.EngineCore
+{
+    using AIEngineConnectivity.Constants;
+    using AIEngineConnectivity.EngineCore;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    public class TemplateProvider : ITemplateProvider
+    {
+        public Task<string> GetTemplate(EngineEvents @event, CancellationToken ct = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
