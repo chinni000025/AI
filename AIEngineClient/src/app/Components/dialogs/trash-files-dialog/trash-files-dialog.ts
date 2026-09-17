346c7f61de3d5d10aa042d00b5f14874e34c07fb
Chinni Veeravalli <ganeshveeravalli25@gmail.com>
2026-09-17 23:30:43 +0530

    Clean up

---
+++ AIEngineClient/src/app/Components/dialogs/trash-files-dialog/trash-files-dialog.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trash-files-dialog',
+  imports: [],
+  templateUrl: './trash-files-dialog.html',
+  styleUrl: './trash-files-dialog.css',
+})
+export class TrashFilesDialog {
+
+}
